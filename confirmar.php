b818bcd4c88ead6fa92d48ea9ecde94338ae75b4
Fernet <fernetsystem@gmail.com>
2015-02-08 06:55:24 -0600

    Logeo del cliente para confirmar compra

---
+++ confirmar.php
@@ -0,0 +1,9 @@
+<?php include "php/cab.inc"; ?>
+<p>Ya eres usuario?</p>
+<form action="" method="POST">
+<input type="text" name="usuario" placeholder="Ingresa tu usuario"/>
+<input type="password" name="password" placeholder="Ingresa tu contraseña"/>
+<input type="submit" />
+</form>
+<p>Eres nuevo usuario?</p>
+<?php include "php/pie.inc"; ?>
