0b78c26e8391ede5a1857a1e10aaca6def9e0981
Fernet <fernetsystem@gmail.com>
2015-02-08 06:06:21 -0600

    mostrar nombre y precio al usuario en el carrito

--- js/codigo.js
+++ js/codigo.js
@@ -2,6 +2,7 @@ $(document).ready(inicio)
 function inicio(){
 	//cuando se ejecute el boton botoncompra ejecutare la funcion anade
 	$(".botoncompra").click(anade)
+	$("#carrito").load("php/poncarrito.php"); //ejecutar cuando carge la pagina el carrito y sin parametros
 }
 function anade(){
 	$("#carrito").load("php/poncarrito.php?p="+$(this).val()); //load es un metodo de ajxa que carga un archivo
--- php/poncarrito.php
+++ php/poncarrito.php
@@ -1,1 +1,18 @@
-<?php echo $_GET['p']; ?>
+<?php 
+	session_start();
+	if(isset($_GET['p'])){ //si existe la variable enviada
+		$_SESSION['producto'][$_SESSION['contador']] = $_GET['p']; //variable de secion de segunda dimension = a el id de la prednda
+		$_SESSION['contador']++; //aumenta de uno en uno el contador
+	}
+	$conexion = mysql_connect("127.0.0.1","root","w9w9dorotea");
+	mysql_select_db("tienda_online",$conexion);
+	for ($i=0; $i<$_SESSION['contador'] ; $i++) { 
+		//echo "Producto: ".$_SESSION['producto'][$i]."<br/>"; 
+		$query = "select * from prendas where id=".$_SESSION['producto'][$i];
+		$resultado = mysql_query($query,$conexion);
+		while ($fila=mysql_fetch_array($resultado)) {
+			echo $fila['nombre']." - ".$fila['precio']." <br/>";
+		}
+	}		
+	mysql_close($conexion);
+ ?>
