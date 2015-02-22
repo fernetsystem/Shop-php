2a6f93b30a715c53a6b2b4756d6fead7217854da
Fernet <fernetsystem@gmail.com>
2015-02-22 06:44:32 -0600

    Correccion de conexion para pedidos

--- php/logcliente.php
+++ php/logcliente.php
@@ -1,34 +1,19 @@
 <?php 
-	include "cab.inc";
+	#include "cab.in
 	$contador = 0;
-	$conexion = mysql_connect("127.0.0.1","root","w9w9dorotea");
-	mysql_select_db("tienda_online");
-	$query = "select * from clientes where usuario='".$_POST['usuario']."' and contrasena = '".$_POST['password']."'";
-	$resultado = mysql_query($conexion,$query);
-	while($fila=mysql_fetch_array($resultado)){
+	#$usua=$_POST['usuario'];
+	#$pass=$_POST['password'];
+	$conexion = mysql_connect("localhost","root","w9w9dorotea");
+	mysql_select_db("tienda_online",$conexion);
+	$q = "select * from clientes where usuario='".$_POST['usuario']."' and password = '".$_POST['password']."'";
+	$resultado = mysql_query($q,$conexion);
+	while ($fila = mysql_fetch_array($resultado)) {
 		$contador++;
-		$_SESSION['usuario'] = $fila['id'];
 	}
 	if($contador > 0){
-		$query = "insert into pedidos values (NULL,".$_SESSION['usuario'].",'".date('U')."','0')";
-		$resultado = mysql_query($conexion,$query);
-		$query = "select * from pedidos where idcliente='".$_SESSION['usuario']."' order by fecha desc limit 1";
-		$resultado = mysql_query($conexion,$query);
-		while ($fila=mysql_fetch_array($resultado)) {
-			$_SESSION['idpedido'] = $fila['id'];
-		}
-		echo $_SESSION['idpedido'];
-		for ($i=0; $i < $_SESSION['contador'] ; $i++) { 
-			#$query = "insert into ventas values(null,'"$_SESSION['idpedido']."','".$_SESSION['producto'][$i]."','1')";
-			$query = "insert into ventas values(null,"$_SESSION['idpedido'].",".$_SESSION['idproducto'][$i].",1)";
-			$resultado = mysql_query($conexion,$query);
-		}
-		include "pie.inc";
-		echo "<p> Tu pedido se ha realiazado exitozamente</p>";
-		echo "<meta http-equiv='refresh' content=*5; url=../index.php >";
+		echo "El usuario existe";
 	}else{
-		echo "El usuario no existe";
+		echo "El usuario NO existe";
 	}
 	mysql_close($conexion);
-	#ALTER TABLE clientes MODIFY id int auto_increment
- ?>
+?>
