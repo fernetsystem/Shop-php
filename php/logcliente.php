9155efa28f0caf45dff5d1936cf5f36c4919e037
Fernet <fernetsystem@gmail.com>
2015-02-22 06:56:22 -0600

    de validar cliente a insertar pedido

--- php/logcliente.php
+++ php/logcliente.php
@@ -1,16 +1,18 @@
 <?php 
 	#include "cab.in
 	$contador = 0;
-	#$usua=$_POST['usuario'];
-	#$pass=$_POST['password'];
 	$conexion = mysql_connect("localhost","root","w9w9dorotea");
 	mysql_select_db("tienda_online",$conexion);
 	$q = "select * from clientes where usuario='".$_POST['usuario']."' and password = '".$_POST['password']."'";
 	$resultado = mysql_query($q,$conexion);
 	while ($fila = mysql_fetch_array($resultado)) {
 		$contador++;
+		$_SESSION['usuario'] = $fila['id'];
 	}
 	if($contador > 0){
+		$q = "insert into pedidos values(NULL,".$_SESSION['usuario'].",'".date('U')."','0')";
+		$resultado = mysql_query($q,$conexion);
+	
 		echo "El usuario existe";
 	}else{
 		echo "El usuario NO existe";
