b0bb029ce152914bf50a92b89d26c922cf2c191d
Fernet <fernetsystem@gmail.com>
2015-03-22 03:20:08 -0600

    Poner la imagen primero de la prenda para volverla despues flotante

--- index1_1.php
+++ index1_1.php
@@ -5,16 +5,17 @@
 	  $resultado = mysql_query($query,$conexion);
 	  while($fila = mysql_fetch_array($resultado)){
 	  	echo "<article>";
+	  	$query2 = "select * from img where idprenda=".$fila['id']." limit 1";
+	  	$resultado2 = mysql_query($query2,$conexion);
+	  	while($fila2 = mysql_fetch_array($resultado2)){
+	  		echo "<img src='photo/".$fila2['imagen']."' >";
+	  	}
 	  	echo "<a href='prenda.php?id=".$fila['id']."'>
 	  			<h3>".$fila['nombre']."</h3>
 	  		  </a>";
 	  	echo "<p>".$fila['descripcion']."</p>";
 	  	echo "<p> Precio: ".$fila['precio']." $</p>";
-	  	$query2 = "select * from img where idprenda=".$fila['id']." limit 1";
-	  	$resultado2 = mysql_query($query2,$conexion);
-	  	while($fila2 = mysql_fetch_array($resultado2)){
-	  		echo "<img src='photo/".$fila2['imagen']."' width=100px>";
-	  	}
+	  	
 	  	echo "<br/>";
 	  	echo "<a href='prenda.php?id=".$fila['id']."'><button>Más información</button></a> ";
 	  	echo "<button value='".$fila['id']."' class='botoncompra'>Comprar ahora</button>";
