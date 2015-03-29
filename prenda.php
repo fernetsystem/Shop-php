158e9b8946fc6b3c2602ce5379f26327dabee896
Fernet <fernetsystem@gmail.com>
2015-03-28 19:29:19 -0600

    Acomodar boton de compra con clase de css

--- prenda.php
+++ prenda.php
@@ -4,12 +4,14 @@
 	  $query = "select * from prendas where id=".$_GET['id']." limit 1";
 	  $resultado = mysql_query($query,$conexion);
 	  while($fila = mysql_fetch_array($resultado)){
-	  	echo "<article>";
+	  	echo "<article class='prendaArt'>";
+	  	echo "<section class='prendaText'>";
 	  	echo "<a href='producto.php?id=".$fila['id']."'>
 	  			<h3>".$fila['nombre']."</h3>
 	  		  </a>";
 	  	echo "<p>".$fila['descripcion']."</p>";
 	  	echo "<p> Precio: ".$fila['precio']." $</p>";
+	  	echo "</section>";
 	  	$query2 = "select * from img where idprenda=".$fila['id']." ";
 	  	$resultado2 = mysql_query($query2,$conexion);
 	  	while($fila2 = mysql_fetch_array($resultado2)){
@@ -18,7 +20,7 @@
 	  	echo "<br/>";
 	  	#echo "<a href='producto.php?id=".$fila['id']."'><button>Más información</button></a>";
 	  	#echo "<button>Comprar ahora</button>";
-	  	echo "<button value='".$fila['id']."' class='botoncompra'>Comprar ahora</button>";
+	  	echo "<button value='".$fila['id']."' class='botoncompra prendaButt'>Comprar ahora</button>";
 	  	echo "</article>";
 	  }
 	  mysql_close($conexion);
