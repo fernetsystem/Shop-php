80dd89ddd0df8e47ce3f594a3fa30b740069af4a
Fernet <fernetsystem@gmail.com>
2015-03-29 02:31:57 -0600

    Login cliente
    Mejorar interfaz del Login

--- confirmar.php
+++ confirmar.php
@@ -1,9 +1,8 @@
 <?php include "php/cab.inc"; ?>
-<p>Ya eres usuario?</p>
 <form action="php/logcliente.php" method="POST" class="formClientLogin">
-<p>Usuario</p><br/>
+<p>Usuario</p>
 <input type="text" name="usuario" placeholder="Ingresa tu usuario"/>
-<p>Password</p><br/>
+<p>Password</p>
 <input type="password" name="password" placeholder="Ingresa tu contraseña"/>
 <br/><br/><br/>
 <!--<input type="tel" name="t" placeholder="Telefono"/><br/>
@@ -11,7 +10,12 @@
 <input type="range" step="10" min="1" max="100"/> <br/>
 <input type="date" min="2012-02-02" max="2015-02-24"/><br/>
 <input tyoe="time"/>-->
-<input type="submit" />
+<input type="submit" value="Iniciar sesión" />
+<br/>
+<br/>
+<p>No dispones de una cuenta de cliente ELITY? <a href="clientes_nuevo.php"> Registrate </a></p>
 </form>
-<p>Eres nuevo usuario? <a href="clientes_nuevo.php"> Registrate </a></p>
+<br/>
+<br/>
+<br/>
 <?php include "php/pie.inc"; ?>
